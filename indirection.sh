e2d051001f81b0676263bc47c5b4802401a77e72
Emma Tosch <etosch@gmail.com>
2011-09-30 11:03:00 -0400

    set indirection script to compress log files and delete originals

--- indirection.sh
+++ indirection.sh
@@ -12,13 +12,17 @@ do
     runsize=( 100 125 150 )
     for ((j=0;j<5;j++))
     do
-	lein run -m examples.lawnmower --size ${lawnsize[j]} --limit ${runsize[j]} --mutation-probability 0.45 --crossover-probability 0.45 1> test_cases/direct_only/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i.out 2> test_cases/direct_only/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i.err
-	echo 	'lein run -m examples.lawnmower --size ${lawnsize[j]} --limit ${runsize[j]} --mutation-probability 0.45 --crossover-probability 0.45 1> test_cases/direct_only/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i.out 2> test_cases/direct_only/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i.err'
-	lein run -m examples.lawnmower --size ${lawnsize[j]} --limit ${runsize[j]} --mutation-probability 0.45 --crossover-probability 0.45 --use-indirection true 1> test_cases/with_indirection/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i.out 2> test_cases/with_indirection/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i.err
-	echo	'lein run -m examples.lawnmower --size ${lawnsize[j]} --limit ${runsize[j]} --mutation-probability 0.45 --crossover-probability 0.45 --use-indirection true 1> test_cases/with_indirection/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i.out 2> test_cases/with_indirection/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i.err'
-	lein run -m examples.dsoar --size ${lawnsize[j]} --limit ${runsize[j]} --mutation-probability 0.45 --crossover-probability 0.45 1> test_cases/dsoar8x${lawnsize[j]}-${runsize[j]}-$i.out 2> test_cases/direct_only/dsoar8x${lawnsize[j]}-${runsize[j]}-$i.err
-	echo	'lein run -m examples.dsoar --size ${lawnsize[j]} --limit ${runsize[j]} --mutation-probability 0.45 --crossover-probability 0.45 1> test_cases/dsoar8x${lawnsize[j]}-${runsize[j]}-$i.out 2> test_cases/direct_only/dsoar8x${lawnsize[j]}-${runsize[j]}-$i.err'
-	lein run -m examples.dsoar --size ${lawnsize[j]} --limit ${runsize[j]} --mutation-probability 0.45 --crossover-probability 0.45 --use-indirection true 1> test_cases/with_indirection/dsoar8x${lawnsize[j]}-${runsize[j]}-$i.out 2> test_cases/with_indirection/dsoar8x${lawnsize[j]}-${runsize[j]}-$i.err
-	echo    'lein run -m examples.dsoar --size ${lawnsize[j]} --limit ${runsize[j]} --mutation-probability 0.45 --crossover-probability 0.45 --use-indirection true 1> test_cases/with_indirection/dsoar8x${lawnsize[j]}-${runsize[j]}-$i.out 2> test_cases/with_indirection/dsoar8x${lawnsize[j]}-${runsize[j]}-$i.err'
+	lein run -m examples.lawnmower --size ${lawnsize[j]} --limit ${runsize[j]} --mutation-probability 0.45 --crossover-probability 0.45 > test_cases/direct_only/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i
+	tar -cjf test_cases/direct_only/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i.tar.gz test_cases/direct_only/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i
+	rm test_cases/direct_only/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i
+	lein run -m examples.lawnmower --size ${lawnsize[j]} --limit ${runsize[j]} --mutation-probability 0.45 --crossover-probability 0.45 --use-indirection true > test_cases/with_indirection/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i
+	tar -cjf test_cases/with_indirection/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i.tar.gz  test_cases/with_indirection/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i
+	rm  test_cases/with_indirection/lawnmower8x${lawnsize[j]}-${runsize[j]}-$i
+	lein run -m examples.dsoar --size ${lawnsize[j]} --limit ${runsize[j]} --mutation-probability 0.45 --crossover-probability 0.45 > test_cases/dsoar8x${lawnsize[j]}-${runsize[j]}-$i
+	tar -cjf test_cases/direct_only/dsoar8x${lawnsize[j]}-${runsize[j]}-$i.tar.gz test_cases/direct_only/dsoar8x${lawnsize[j]}-${runsize[j]}-$i
+	rm test_cases/direct_only/dsoar8x${lawnsize[j]}-${runsize[j]}-$i
+	lein run -m examples.dsoar --size ${lawnsize[j]} --limit ${runsize[j]} --mutation-probability 0.45 --crossover-probability 0.45 --use-indirection true > test_cases/with_indirection/dsoar8x${lawnsize[j]}-${runsize[j]}-$i
+	tar -cjf test_cases/with_indirection/dsoar8x${lawnsize[j]}-${runsize[j]}-$i.tar.gz test_cases/with_indirection/dsoar8x${lawnsize[j]}-${runsize[j]}-$i
+	rm test_cases/with_indirection/dsoar8x${lawnsize[j]}-${runsize[j]}-$i.err
     done
 done
