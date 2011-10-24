7257963d171bdf0dee6ad3544e6f710fa98adc24
Emma Tosch <etosch@gmail.com>
2011-10-24 09:43:47 -0400

    updated and tested with 1.3

--- src/examples/argmap_regression.clj
+++ src/examples/argmap_regression.clj
@@ -1,6 +1,6 @@
 (ns examples.argmap-regression
-  (:require [clojush] [clojure.contrib.math])
-  (:use [clojush] [clojure.contrib.math]))
+  (:require [clojush]
+	    [clojure.math.numeric-tower :as math]))
 
 ;; argmap_regression.clj
 ;; an example problem for clojush, a Push/PushGP system written in Clojure
@@ -14,27 +14,29 @@
 ;; trivial geography chapter) with minimal integer instructions and an 
 ;; input instruction that uses the auxiliary stack.
 
-(define-registered in 
-  (fn [state] (push-item (stack-ref :auxiliary 0 state) :integer state)))
+(clojush/define-registered in 
+  (fn [state] (clojush/push-item (clojush/stack-ref :auxiliary 0 state) :integer state)))
 
-(pushgp-map
-  {:error-function (fn [program]
-                     (doall
-                       (for [input (range 10)]
-                         (let [state (run-push program 
-                                       (push-item input :auxiliary 
-                                         (push-item input :integer 
-                                           (make-push-state))))
-                               top-int (top-item :integer state)]
-                           (if (number? top-int)
-                             (abs (- top-int 
-                                    (- (* input input input) 
-                                      (* 2 input input) input)))
-                             1000)))))
-   :atom-generators (list (fn [] (rand-int 10))
-                      'in
-                      'integer_div
-                      'integer_mult
-                      'integer_add
-                      'integer_sub)
-   :tournament-size 3})
+(defn run [args]
+  (clojush/pushgp-map
+   (-> args
+       (assoc :error-function (fn [program]
+				(doall
+				 (for [input (range 10)]
+				   (let [state (clojush/run-push program 
+								 (clojush/push-item input :auxiliary 
+										    (clojush/push-item input :integer 
+												       (clojush/make-push-state))))
+					 top-int (clojush/top-item :integer state)]
+				     (if (number? top-int)
+				       (math/abs (- top-int 
+					       (- (* input input input) 
+						  (* 2 input input) input)))
+				       1000))))))
+       (assoc :atom-generators (list (fn [] (rand-int 10))
+				     'in
+				     'integer_div
+				     'integer_mult
+				     'integer_add
+				     'integer_sub))
+       (assoc :tournament-size 3))))
