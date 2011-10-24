a65da5026495dba50b0556e3ab7d6a32815638d3
Emma Tosch <etosch@gmail.com>
2011-10-24 09:50:50 -0400

    updated and tested with 1.3

--- src/examples/gaussian_mutation_demo.clj
+++ src/examples/gaussian_mutation_demo.clj
@@ -3,8 +3,8 @@
 ;; Lee Spector, lspector@hampshire.edu, 2011
 
 (ns examples.gaussian-mutation-demo
-  (:require [clojush] [clojure.contrib.math])
-  (:use [clojush] [clojure.contrib.math]))
+  (:require [clojush]
+	    [clojure.math.numeric-tower :as math]))
 
 ;;;;;;;;;;;;
 ;; Floating point symbolic regression of a polynomial that uses non-integral 
@@ -15,37 +15,39 @@
 ;; arithmetic manipulation of integral values, it is probably easier to 
 ;; produce it through gaussian mutations.
 
-(define-registered in 
-  (fn [state] (push-item (stack-ref :auxiliary 0 state) :float state)))
+(clojush/define-registered in 
+  (fn [state] (clojush/push-item (clojush/stack-ref :auxiliary 0 state) :float state)))
 
-(pushgp 
-  :error-function (fn [program]
-                    (doall
-                      (for [input (range -1.0 1.0 0.1)]
-                        (let [state (run-push program 
-                                      (push-item input :auxiliary 
-                                        (push-item input :float
-                                          (make-push-state))))
-                              top-float (top-item :float state)
+(defn run [args]
+  (clojush/pushgp-map
+   (-> args
+       (assoc :error-function (fn [program]
+				(doall
+				 (for [input (range -1.0 1.0 0.1)]
+				   (let [state (clojush/run-push program 
+                                      (clojush/push-item input :auxiliary 
+                                        (clojush/push-item input :float
+                                          (clojush/make-push-state))))
+                              top-float (clojush/top-item :float state)
                               invalid-output (or (not (number? top-float))
                                                (= (:termination state) :abnormal))]
                           (if invalid-output
                             1000
-                            (abs (- top-float
+                            (math/abs (- top-float
                                    (+ (* 1.23 input input)
-                                     0.73))))))))
-  :atom-generators (concat 
-                     '(float_div float_mult float_sub float_add
-                        float_rot float_swap float_dup float_pop)
-                     (list 
-                       (fn [] (* 1.0 (- (rand-int 21) 10)))
-                       'in))
-  :mutation-probability 0.2
-  :crossover-probability 0.2
-  :simplification-probability 0.2
-  :reproduction-simplifications 10
-  :gaussian-mutation-probability 0.3
-  :gaussian-mutation-per-number-mutation-probability 1.0
-  :gaussian-mutation-standard-deviation 0.1
-  :error-threshold 1.0
-  :population-size 5000)
+				      0.73)))))))))
+       (assoc :atom-generators (concat 
+				'(float_div float_mult float_sub float_add
+					    float_rot float_swap float_dup float_pop)
+				(list 
+				 (fn [] (* 1.0 (- (rand-int 21) 10)))
+				 'in)))
+       (assoc :mutation-probability 0.2)
+       (assoc :crossover-probability 0.2)
+       (assoc :simplification-probability 0.2)
+       (assoc :reproduction-simplifications 10)
+       (assoc :gaussian-mutation-probability 0.3)
+       (assoc :gaussian-mutation-per-number-mutation-probability 1.0)
+       (assoc :gaussian-mutation-standard-deviation 0.1)
+       (assoc :error-threshold 1.0)
+       (assoc :population-size 5000))))
